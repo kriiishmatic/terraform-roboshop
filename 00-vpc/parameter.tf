b0aa1b4628441c5f94f758c3bc421324c704f5f1
kriiishamtic <kriiishmatic@gmail.com>
2025-10-28 01:16:31 +0530

    Your commit message

--- 00-vpc/parameter.tf
+++ 00-vpc/parameter.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "vpc_id" {
-  name  = "local.common_name_prefix-vpc_id"
+  name  = "${local.common_name_prefix}-vpc_id" #roboshop-devp-vpc_id
   type  = "String"
   value = module.vpc.vpc_id
 }
