a9da85260524967166f6501698f200a1c0dfbcec
kriiishamtic <kriiishmatic@gmail.com>
2025-11-12 17:21:02 +0530

    all

--- 20-bastion/terraform-i.sh
+++ 20-bastion/terraform-i.sh
@@ -7,3 +7,11 @@ xfs_growfs /home
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
+
+## creating databases
+cd home/ec2-user
+git clone https://github.com/kriiishmatic/terraform-roboshop.git
+chown -R ec2-user:ec2-user terraform-roboshop
+cd terraform-roboshop/40-database
+terraform init
+terraform apply -auto-approve
