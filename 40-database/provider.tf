f9bb81f2846a7e20f0d7039d415f840028f9cc08
kriiishamtic <kriiishmatic@gmail.com>
2025-11-12 18:27:16 +0530

    all

--- 40-database/provider.tf
+++ 40-database/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "terraform-state-kriiishmatic"
-    key    = "vpc-roboshop-database-th-bastion"
+    key    = "vpc-roboshop-database-demo"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
