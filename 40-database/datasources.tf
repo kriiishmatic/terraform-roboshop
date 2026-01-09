f48f8b4396d9e1f7297a4ad784754577da29ccb6
kriiishamtic <kriiishmatic@gmail.com>
2026-01-09 20:42:03 +0530

    all

--- 40-database/datasources.tf
+++ 40-database/datasources.tf
@@ -19,13 +19,13 @@ data "aws_ami" "ami_id" {
 }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
-  name = "/${var.project}/${var.environment}/mongodb_sg_ids"
+  name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
 data "aws_ssm_parameter" "redis_sg_id" {
-  name = "/${var.project}/${var.environment}/redis_sg_ids"
+  name = "/${var.project}/${var.environment}/redis_sg_id"
 }
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
-  name = "/${var.project}/${var.environment}/rabbitmq_sg_ids"
+  name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 }
 
 data "aws_ssm_parameter" "database_subnet_ids" {
@@ -33,7 +33,7 @@ data "aws_ssm_parameter" "database_subnet_ids" {
 }
 
 data "aws_ssm_parameter" "mysql_sg_id" {
-  name = "/${var.project}/${var.environment}/mysql_sg_ids"
+  name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
 
 # data from aws-zone
